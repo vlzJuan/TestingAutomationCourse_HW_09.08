28a07052288a1b5c4c086d2c1b8d67afad6fcd5e
vlzJuan <valencellojuan@gmail.com>
2024-08-07 16:03:29 -0300

    [BubbleSortApp]: -Added the ```main``` function with an empty body for now. -Added the function ```arrayPrinter(int[], String)```, which outputs to standard output a descriptive message that includes a String associated with the array, and the array. -Created an int[] variable, ```ordenable```, initialized as an array of 10 random ints using ```randomArrayOfInts(10)``` -Created the code required to perform a bubble sort on said ```ordenable``` array.

--- src/BubbleSortApp.java
+++ src/BubbleSortApp.java
@@ -12,11 +12,34 @@ public class BubbleSortApp {
     /**
      * Main function, for the execution of the program.
      * Here, I will perform a bubble sort on the array of random numbers
-     * returned by '''randomArrayOfInts'''
+     * returned by 'randomArrayOfInts', to sort it in descending order.
      *
      * @param args ;
      */
     public static void main(String[] args){
+
+        // Implement a fixed array to test the program instead of the random one.
+
+        int[] ordenable = randomArrayOfInts(10);    //  Build a new array
+                                                    //  of 10 random ints.
+        arrayPrinter(ordenable, "Original array:");
+
+        // Send this to a subroutine later.
+        for(int i=0; i<ordenable.length; i++){
+            for(int j = i+1; j<ordenable.length; j++){
+                //  Condition for sorting: If the j-th element is larger
+                //  than the i-th, swap them.
+                //  By doing this, the larger values will 'bubble up' to
+                //  the first elements of the array.
+                if(ordenable[j] > ordenable[i]){
+                    int aux = ordenable[j];
+                    ordenable[j] = ordenable[i];
+                    ordenable[i] = aux;
+                }
+            }
+        }
+
+        arrayPrinter(ordenable, "Array after bubblesort (Descending)")
 
 
     }
@@ -44,5 +67,28 @@ public class BubbleSortApp {
 
         return array;
     }
+
+
+    /**
+     * Subroutine used to show the values of an int Array, as well as a
+     * descriptive message associated to said array.
+     *
+     * @param array , an array of ints.
+     * @param msj   , a message to print before printing the array.
+     */
+    void arrayPrinter(int[] array, String msj){
+
+        System.out.println(msj);
+
+        System.out.print("[ ");
+        for(int value:array){
+            System.out.println( valor + " ");
+        }
+
+        System.out.print("]");
+
+    }
+
+
 
 }
